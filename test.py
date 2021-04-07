9b340c2354d00dd9a153ffd7e6460a64bdc295d9
Pubudu Arosha <it18108514@my.sliit.lk>
2021-04-07 16:00:45 +0530

    add the project

---
+++ test.py
@@ -0,0 +1,31 @@
+import cv2 
+import time
+import uuid
+import os
+
+IMAGES_PATH = 'Tensorflow/workspace/images/collectedimages'
+
+lables = ['hello', 'thanks', 'yes', 'no', 'iloveyou']
+number_imgs = 15
+
+for lable in lables:
+
+    !mkdir {'Tensorflow\workspace\images\collectedimages\\'+lable}
+
+    cap = cv2.VideoCapture(0)
+    print('Collecting imagess for {}'.format(lables))
+    time.sleep(5)
+    for imgnum in range(number_imgs):
+        ret, frame = cap.read()
+        imgname = os.path.join(IMAGES_PATH, lable, lable+'.'+'{}.jpg'.format(str(uuid.uuid1())))
+        cv2.imwrite(imgname, frame)
+        cv2.imshow('frame', frame)
+        time.sleep()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    cap.release()
+
+
+
